31f866673c9440874781d05c5c9c70c194f6bc24
sqayum <sqayum33@gmail.com>
2021-11-02 12:35:23 -0500

    final commit

--- nlp_utils.py
+++ nlp_utils.py
@@ -1,19 +1,31 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from nltk.probability import FreqDist
-import itertools
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import validation_curve
+from matplotlib.gridspec import GridSpec
+from matplotlib.ticker import MultipleLocator
+
 
 CONTRACTION_MAP = {
     "ain't": "is not",
     "aren't": "are not",
-    "can't": "cannot",
+    "can't": "can not",
     "'cause": "because",
+    "cha": "you",
+    "coulda": "could have",
     "could've": "could have",
     "couldn't": "could not",
     "didn't": "did not",
     "doesn't": "does not",
     "don't": "do not",
+    "dunno": "do not know",
+    "gimme": "give me",
+    "gonna": "going to",
+    "gotta": "got to",
+    "gotcha": "got you",
     "hadn't": "had not",
     "hasn't": "has not",
     "haven't": "have not",
@@ -27,11 +39,14 @@ CONTRACTION_MAP = {
     "i'd": "i would",
     "i'll": "i will",
     "i'm": "i am",
+    "imma": "i am",
     "i've": "i have",
     "isn't": "is not",
     "it'd": "it would",
     "it'll": "it will",
     "it's": "it is",
+    "kinda": "kind of",
+    "lemme": "let me",
     "let's": "let us",
     "ma'am": "madam",
     "mayn't": "may not",
@@ -43,11 +58,13 @@ CONTRACTION_MAP = {
     "not've": "not have",
     "o'clock": "of the clock",
     "oughtn't": "ought not",
+    "outta": "out of",
     "shan't": "shall not",
     "sha'n't": "shall not",
     "she'd": "she would",
     "she'll": "she will",
     "she's": "she is",
+    "shoulda": "should have",
     "should've": "should have",
     "shouldn't": "should not",
     "so've": "so have",
@@ -61,6 +78,7 @@ CONTRACTION_MAP = {
     "they're": "they are",
     "they've": "they have",
     "to've": "to have",
+    "wanna": "want to",
     "wasn't": "was not",
     "we'd": "we would",
     "we'll": "we will",
@@ -83,8 +101,10 @@ CONTRACTION_MAP = {
     "why've": "why have",
     "will've": "will have",
     "won't": "will not",
+    "woulda": "would have",
     "would've": "would have",
     "wouldn't": "would not",
+    "tryna": "trying to",
     "y'all": "you all",
     "y'all'd": "you all would",
     "y'all'd've": "you all would have",
@@ -93,115 +113,89 @@ CONTRACTION_MAP = {
     "you'd": "you would",
     "you'll": "you will",
     "you're": "you are",
-    "you've": "you have"}
+    "you've": "you have",
+    "u": "you",
+    "ya": "you"}
 
-LANGUAGE_CODES = {
- 'af': 'afrikaans',
- 'sq': 'albanian',
- 'am': 'amharic',
- 'ar': 'arabic',
- 'hy': 'armenian',
- 'az': 'azerbaijani',
- 'eu': 'basque',
- 'be': 'belarusian',
- 'bn': 'bengali',
- 'bs': 'bosnian',
- 'bg': 'bulgarian',
- 'ca': 'catalan',
- 'ceb': 'cebuano',
- 'ny': 'chichewa',
- 'zh-cn': 'chinese (simplified)',
- 'zh-tw': 'chinese (traditional)',
- 'co': 'corsican',
- 'hr': 'croatian',
- 'cs': 'czech',
- 'da': 'danish',
- 'nl': 'dutch',
- 'en': 'english',
- 'eo': 'esperanto',
- 'et': 'estonian',
- 'tl': 'filipino',
- 'fi': 'finnish',
- 'fr': 'french',
- 'fy': 'frisian',
- 'gl': 'galician',
- 'ka': 'georgian',
- 'de': 'german',
- 'el': 'greek',
- 'gu': 'gujarati',
- 'ht': 'haitian creole',
- 'ha': 'hausa',
- 'haw': 'hawaiian',
- 'iw': 'hebrew',
- 'hi': 'hindi',
- 'hmn': 'hmong',
- 'hu': 'hungarian',
- 'is': 'icelandic',
- 'ig': 'igbo',
- 'id': 'indonesian',
- 'ga': 'irish',
- 'it': 'italian',
- 'ja': 'japanese',
- 'jw': 'javanese',
- 'kn': 'kannada',
- 'kk': 'kazakh',
- 'km': 'khmer',
- 'ko': 'korean',
- 'ku': 'kurdish (kurmanji)',
- 'ky': 'kyrgyz',
- 'lo': 'lao',
- 'la': 'latin',
- 'lv': 'latvian',
- 'lt': 'lithuanian',
- 'lb': 'luxembourgish',
- 'mk': 'macedonian',
- 'mg': 'malagasy',
- 'ms': 'malay',
- 'ml': 'malayalam',
- 'mt': 'maltese',
- 'mi': 'maori',
- 'mr': 'marathi',
- 'mn': 'mongolian',
- 'my': 'myanmar (burmese)',
- 'ne': 'nepali',
- 'no': 'norwegian',
- 'ps': 'pashto',
- 'fa': 'persian',
- 'pl': 'polish',
- 'pt': 'portuguese',
- 'pa': 'punjabi',
- 'ro': 'romanian',
- 'ru': 'russian',
- 'sm': 'samoan',
- 'gd': 'scots gaelic',
- 'sr': 'serbian',
- 'st': 'sesotho',
- 'sn': 'shona',
- 'sd': 'sindhi',
- 'si': 'sinhala',
- 'sk': 'slovak',
- 'sl': 'slovenian',
- 'so': 'somali',
- 'es': 'spanish',
- 'su': 'sundanese',
- 'sw': 'swahili',
- 'sv': 'swedish',
- 'tg': 'tajik',
- 'ta': 'tamil',
- 'te': 'telugu',
- 'th': 'thai',
- 'tr': 'turkish',
- 'uk': 'ukrainian',
- 'ur': 'urdu',
- 'uz': 'uzbek',
- 'vi': 'vietnamese',
- 'cy': 'welsh',
- 'xh': 'xhosa',
- 'yi': 'yiddish',
- 'yo': 'yoruba',
- 'zu': 'zulu',
- 'fil': 'filipino',
- 'he': 'hebrew'}
+CONVERSATIONAL_ABBREVIATION_MAP = {
+    "afaik": "as far as i know",
+    "bc": "because",
+    "bfn": "bye for now",
+    "brb": "be right back",
+    "btw": "by the way",
+    "dm": "direct message",
+    "dyk": "did you know",
+    "fomo": "fear of missing out",
+    "fb": "facebook",
+    "fml": "fuck my life",
+    "fr": "for real",
+    "ftf": "face to face",
+    "ftl": "for the loss",
+    "ftw": "for the win",
+    "fwd": "forward",
+    "fwiw": "for what it is worth",
+    "fyi": "for your information",
+    "gtg": "got to go",
+    "gtr": "got to run",
+    "hifw": "how i feel when",
+    "hmb": "hit me back",
+    "hmu": "hit me up",
+    "hth": "hope that helps",
+    "idc": "i do not care",
+    "idk": "i do not know",
+    "ikr": "i know right",
+    "ily": "i love you",
+    "imho": "in my humble opinion",
+    "imo": "in my opinion",
+    "irl": "in real life",
+    "jk": "just kidding",
+    "lmao": "laughing my ass off",
+    "lmk": "let me know",
+    "lol": "laughing out loud",
+    "nbd": "no big deal",
+    "nm": "not much",
+    "nfw": "no fucking way",
+    "nsfw": "not safe for work",
+    "nvm": "nevermind",
+    "omfg": "oh my fucking God",
+    "omg": "oh my God",
+    "omw": "on my way",
+    "ppl": "people",
+    "rly": "really",
+    "rofl": "rolling on the floor laughing",
+    "sfw": "safe for work",
+    "smh": "shaking my head",
+    "stfu": "shut the fuck up",
+    "tbh": "to be honest",
+    "tfw": "that feeling when",
+    "tgif": "thank God its Friday",
+    "tmi": "too much information",
+    "tldr": "too long did not read",
+    "wbu": "what about you",
+    "wtf": "what the fuck",
+    "wth": "what the hell",
+    "ty": "thank you",
+    "txt": "text",
+    "yolo": "you only live once",
+    "yw": "your welcome",
+    "zomg": "oh my God"}
+
+
+TECHNICAL_ABBREVIATION_MAP = {
+    "DM": "direct message",
+    "CT": "cuttweet",
+    "RT": "retweet",
+    "MT": "modified tweet",
+    "HT": "hat tip",
+    "CC": "carbon-copy",
+    "CX": "correction",
+    "FB": "Facebook",
+    "LI": "LinkedIn",
+    "YT": "YouTube"}
+
+
+
+
 
 def _gather_tokens(tokenized_documents, by_document=False):
     for token_list in tokenized_documents:
@@ -210,17 +204,22 @@ def _gather_tokens(tokenized_documents, by_document=False):
         for token in token_list:
             yield token
 
-def get_document_frequencies(corpus, label=None, *, N=20):
-    if isinstance(corpus.tokens[0], str):
-        corpus.tokens = corpus.tokens.apply(str.split)
+def get_document_frequencies(corpus, label=None, *, N=None):
     if label is not None:
-        corpus = corpus.loc[corpus["label"] == label]
+        corpus_tokens = corpus.loc[corpus["label"] == label, "tokens"].reset_index(drop=True)
     else:
-        label="ALL"
+        corpus_tokens = corpus.tokens
+        label = "ALL"
 
-    gathered_tokens = _gather_tokens(corpus.tokens, by_document=True)
+    if isinstance(corpus_tokens.values[0], str):
+        corpus_tokens = corpus_tokens.apply(str.split)
+
+    gathered_tokens = _gather_tokens(corpus_tokens, by_document=True)
     frequency_df = pd.DataFrame(FreqDist(gathered_tokens).items(), columns=['word', 'frequency'])
     frequency_df.sort_values('frequency', inplace=True)
+
+    if N is None:
+        N = corpus.shape[0]
 
     total_num_tokens = frequency_df["frequency"].sum()
     tokens = frequency_df["word"].tail(N)
@@ -255,100 +254,143 @@ def average_token_length(corpus):
         corpus.tokens = corpus.tokens.apply(str.split)
     return round(corpus["tokens"].map(len).mean(), 2)
 
-def plot_confusion_matrix(cf,
-                          group_names=None,
-                          categories='auto',
-                          count=True,
-                          percent=True,
-                          cbar=True,
-                          xyticks=True,
-                          xyplotlabels=True,
-                          sum_stats=True,
-                          figsize=None,
-                          cmap='Blues',
-                          title=None):
-    '''
-    This function will make a pretty plot of an sklearn Confusion Matrix cm using a Seaborn heatmap visualization.
-    Arguments
-    ---------
-    cf:            confusion matrix to be passed in
-    group_names:   List of strings that represent the labels row by row to be shown in each square.
-    categories:    List of strings containing the categories to be displayed on the x,y axis. Default is 'auto'
-    count:         If True, show the raw number in the confusion matrix. Default is True.
-    normalize:     If True, show the proportions for each category. Default is True.
-    cbar:          If True, show the color bar. The cbar values are based off the values in the confusion matrix.
-                   Default is True.
-    xyticks:       If True, show x and y ticks. Default is True.
-    xyplotlabels:  If True, show 'True Label' and 'Predicted Label' on the figure. Default is True.
-    sum_stats:     If True, display summary statistics below the figure. Default is True.
-    figsize:       Tuple representing the figure size. Default will be the matplotlib rcParams value.
-    cmap:          Colormap of the values displayed from matplotlib.pyplot.cm. Default is 'Blues'
-                   See http://matplotlib.org/examples/color/colormaps_reference.html
 
-    title:         Title for the heatmap. Default is None.
-    '''
+def get_corpus_stop_words(corpus, *,  threshold):
+    positive_frequency_dict = get_document_frequencies(corpus, "POSITIVE")
+    positive_stop_words = {word for word in positive_frequency_dict if positive_frequency_dict[word] >= threshold}
+
+    neutral_frequency_dict = get_document_frequencies(corpus, "NEUTRAL")
+    neutral_stop_words = {word for word in neutral_frequency_dict if neutral_frequency_dict[word] >= threshold}
+
+    negative_frequency_dict = get_document_frequencies(corpus, "NEGATIVE")
+    negative_stop_words = {word for word in negative_frequency_dict if negative_frequency_dict[word] >= threshold}
+
+    return (positive_stop_words & neutral_stop_words & negative_stop_words)
 
 
-    # CODE TO GENERATE TEXT INSIDE EACH SQUARE
-    blanks = ['' for i in range(cf.size)]
 
-    if group_names and len(group_names)==cf.size:
-        group_labels = ["{}\n".format(value) for value in group_names]
+def plot_confusion_matrices(y_train_true, y_train_pred,
+                             y_validate_true, y_validate_pred,
+                             *,
+                             labels=None,
+                             sample_weights=None,
+                             normalize=None,
+                             cbar=False,
+                             figsize=None):
+
+
+    cm_train = confusion_matrix(y_train_true, y_train_pred, sample_weight=sample_weights, normalize=normalize)
+    cm_validate = confusion_matrix(y_validate_true, y_validate_pred, normalize=normalize)
+
+    cm_train_values = [f"{round(value, 2):.2f}" for value in cm_train.flatten()]
+    cm_train_labels = np.asarray(cm_train_values).reshape(cm_train.shape[0], cm_train.shape[1])
+
+    cm_validate_values = [f"{round(value, 2):.2f}" for value in cm_validate.flatten()]
+    cm_validate_labels = np.asarray(cm_validate_values).reshape(cm_validate.shape[0], cm_validate.shape[1])
+
+    if figsize is None:
+        figsize = (16, 7)
+
+    plt.rcParams['xtick.labelsize'] = 13
+    plt.rcParams['ytick.labelsize'] = 13
+    plt.rcParams['axes.labelsize'] = 16
+    plt.rcParams['axes.titlesize'] = 19
+
+    fig = plt.figure(figsize=figsize)
+    gs = GridSpec(1, 2, figure=fig)
+    gs.update(wspace=0.4)
+
+    ax1 = fig.add_subplot(gs[0,0])
+    sns.heatmap(cm_train, annot=cm_train_labels, fmt="", cmap="Blues", cbar=cbar, xticklabels=labels, yticklabels=labels, ax=ax1)
+    ax1.set(title="Confusion Matrix (Train)", ylabel = 'True Label', xlabel = 'Predicted Label')
+
+    ax2 = fig.add_subplot(gs[0,1])
+    sns.heatmap(cm_validate, annot=cm_validate_labels, fmt="", cmap="Oranges", cbar=cbar, xticklabels=labels, yticklabels=labels, ax=ax2)
+
+    ax2.set(title="Confusion Matrix (Validation)", ylabel = 'True Label', xlabel = 'Predicted Label')
+
+def plot_validation_curve(estimator, X_train, y_train, *, param_name, param_range, scoring, scoring_label, fit_params=None, cv=5, semilogx=False, n_jobs=-1):
+    from sklearn.model_selection import validation_curve
+
+    estimator_name = str(estimator)
+
+    train_scores, test_scores = validation_curve(estimator, X_train, y_train,
+                                                 param_name=param_name,
+                                                 param_range=param_range,
+                                                 scoring=scoring,
+                                                 fit_params=fit_params,
+                                                 n_jobs=n_jobs)
+
+    avg_train_scores = np.array([np.average(train_scores[i,:]) for i in range(train_scores.shape[0])])
+    avg_test_scores = np.array([np.average(test_scores[i,:]) for i in range(test_scores.shape[0])])
+
+    plt.rcParams['axes.labelsize'] = 15
+    plt.rcParams['axes.titlesize'] = 18
+    fig, ax = plt.subplots(figsize=(16,7))
+    if not semilogx:
+        ax.plot(param_range, avg_train_scores, param_range, avg_test_scores, param_range, np.abs(avg_train_scores - avg_test_scores), 'r--')
     else:
-        group_labels = blanks
+        ax.semilogx(param_range, avg_train_scores, param_range, avg_test_scores, param_range, np.abs(avg_train_scores - avg_test_scores), 'r--')
+    ax.set(title=f'Validation Metrics vs {param_name} [{estimator_name}]',
+        xlabel=param_name,
+        ylabel=scoring_label,
+        ylim=(0,1))
+    ax.legend(['Train', 'Validate', 'Deviation'])
+    plt.show()
 
-    if count:
-        group_counts = ["{0:0.0f}\n".format(value) for value in cf.flatten()]
-    else:
-        group_counts = blanks
+def _scale_bar_width(ax, factor):
+    from math import isclose
+    sorted_patches = sorted(ax.patches, key=lambda x: x.get_x())
+    for i, patch in enumerate(sorted_patches):
+        current_width = patch.get_width()
 
-    if percent:
-        group_percentages = ["{0:.2%}".format(value) for value in cf.flatten()/np.sum(cf)]
-    else:
-        group_percentages = blanks
+        updated_current_width = factor * current_width
+        patch.set_width(updated_current_width)
 
-    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels,group_counts,group_percentages)]
-    box_labels = np.asarray(box_labels).reshape(cf.shape[0],cf.shape[1])
+        if i == len(sorted_patches) - 1:
+            return
+
+        current_x = patch.get_x()
+        next_x = sorted_patches[i+1].get_x()
+        if isclose(current_x+current_width, next_x, rel_tol=1e-7, abs_tol=1e-7):
+            patch.set_x(next_x - updated_current_width)
+
+def _get_validation_metric(train_metrics_dict , validate_metrics_dict, *, score_name, score_label, target_names):
+    train_metric_df = pd.DataFrame(train_metrics_dict).loc[score_name, target_names]
+    train_metric_df.name = f"Train (Accuracy = ${round(train_metrics_dict['accuracy'], 3)}$)"
+
+    validate_metric_df = pd.DataFrame(validate_metrics_dict).loc[score_name,target_names]
+    validate_metric_df.name = f"Validate (Accuracy = ${round(validate_metrics_dict['accuracy'], 3)}$)"
+
+    return pd.concat([train_metric_df, validate_metric_df], axis=1).stack().reset_index().rename(columns={'level_0': 'Label', 'level_1': 'Dataset', 0: score_label})
 
 
-    # CODE TO GENERATE SUMMARY STATISTICS & TEXT FOR SUMMARY STATS
-    if sum_stats:
-        #Accuracy is sum of diagonal divided by total observations
-        accuracy  = np.trace(cf) / float(np.sum(cf))
+def plot_validation_metrics(y_train, y_train_pred, y_validate, y_validate_pred, *, score_names, target_names, estimator_label, sample_weight):
 
-        #if it is a binary confusion matrix, show some more stats
-        if len(cf)==2:
-            #Metrics for Binary Confusion Matrices
-            precision = cf[1,1] / sum(cf[:,1])
-            recall    = cf[1,1] / sum(cf[1,:])
-            f1_score  = 2*precision*recall / (precision + recall)
-            stats_text = "\n\nAccuracy={:0.3f}\nPrecision={:0.3f}\nRecall={:0.3f}\nF1 Score={:0.3f}".format(
-                accuracy,precision,recall,f1_score)
+    N = len(score_names)
+    fig, axes = plt.subplots(figsize=(15,N*6), nrows=N)
+
+    train_metrics_dict = classification_report(y_train, y_train_pred, target_names=target_names, sample_weight=sample_weight, output_dict=True)
+    validate_metrics_dict = classification_report(y_validate, y_validate_pred, target_names=target_names, output_dict=True)
+
+    for i, score_name in enumerate(score_names):
+        if '-' in score_name:
+            score_label = '-'.join([x.capitalize() for x in score_name.split('-')])
         else:
-            stats_text = "\n\nAccuracy={:0.3f}".format(accuracy)
-    else:
-        stats_text = ""
+            score_label = score_name.capitalize()
+        score_df = _get_validation_metric(train_metrics_dict , validate_metrics_dict, score_name=score_name, score_label=score_label, target_names=target_names)
+        sns.barplot(x=score_df["Label"], y=score_df[score_label], hue=score_df["Dataset"], ax=axes[i])
+        axes[i].set(xlabel=None, ylim=(0,1))
+        _scale_bar_width(axes[i], 0.55)
+        axes[i].yaxis.set_major_locator(MultipleLocator(base=0.1))
+        if i == 0:
+            axes[i].set(title=f"Validation Metrics by Label [{estimator_label}]")
+            axes[i].legend(loc="best")
+        else:
+            axes[i].get_legend().remove()
 
-
-    # SET FIGURE PARAMETERS ACCORDING TO OTHER ARGUMENTS
-    if figsize==None:
-        #Get default figure size if not set
-        figsize = plt.rcParams.get('figure.figsize')
-
-    if xyticks==False:
-        #Do not show categories if xyticks is False
-        categories=False
-
-
-    # MAKE THE HEATMAP VISUALIZATION
-    plt.figure(figsize=figsize)
-    sns.heatmap(cf,annot=box_labels,fmt="",cmap=cmap,cbar=cbar,xticklabels=categories,yticklabels=categories)
-
-    if xyplotlabels:
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label' + stats_text)
-    else:
-        plt.xlabel(stats_text)
-
-    if title:
-        plt.title(title)
+def get_classification_metrics(y_true, y_pred, *, target_names, sample_weight=None):
+    metrics_df = pd.DataFrame(classification_report(y_true, y_pred, target_names=target_names, sample_weight=sample_weight, output_dict=True)).loc["precision":"f1-score", ["NEGATIVE", "NEUTRAL", "POSITIVE", "weighted avg"]].apply(lambda x: round(x, 3))
+    metrics_df.index = ["Precision", "Recall", "F1-Score"]
+    metrics_df.columns = ["NEGATIVE", "NEUTRAL", "POSITIVE", "Weighted Average"]
+    return metrics_df
